67d83263eb49fe21412182b83981d0bd91ec7014
Joshua23-max <joshuashereni17@gmail.com>
2026-09-22 12:37:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import { GoogleGenAI } from "@google/genai";
 
 dotenv.config();
@@ -12,7 +14,15 @@ const ai = new GoogleGenAI({
 });
 
 app.use(express.json());
-app.use(express.static("public"));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const characters = {
   elara: {
